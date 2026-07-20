74c9dd7c54c46912deae24dbf9f6059cda5d6be8
Yakir <yakir0391@gmail.com>
2026-07-20 19:27:40 +0300

    add infra

---
+++ MatchmakingService/Shared.Infrastructure/Messaging/RabbitMQ/Interfaces/IRabbitMqPublisher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Infrastructure.Messaging.RabbitMQ.Interfaces
+{
+    public interface IRabbitMqPublisher
+    {
+        Task PublishAsync<T>(string queueName, T message);
+    }
+}
---
+++ MatchmakingService/Shared.Infrastructure/Messaging/RabbitMQ/Options/RabbitMqOptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Infrastructure.Messaging.RabbitMQ.Options
+{
+    public class RabbitMqOptions
+    {
+        public const string SectionName = "RabbitMQ";
+
+        public string Host { get; set; } = string.Empty;
+
+        public string User { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+    }
+}
